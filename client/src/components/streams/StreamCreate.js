1489e529ba55056a6e4ffca4bc1aac5cbf3d2a24
adafia <adafia.samuel@gmail.com>
2020-03-23 13:19:35 +0200

    add data persistence with json-api  - wire up redux thunk  - create axios instance  - add all redux action creators for the application [Delivers create-streams-json-api]

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import { createStore } from 'redux';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -24,8 +27,8 @@ class StreamCreate extends Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues)
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
   render() {
     return (
@@ -49,7 +52,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
